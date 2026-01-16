8759d29c82c267ea1b10f6229617cd268a2f9a2b
Yurii Hulyk <URiiGulick32@gmail.com>
2026-01-16 14:33:34 +0000

    feat(zod): apiser zod

---
+++ packages/zod/src/index.ts
@@ -0,0 +1,40 @@
+import z from "zod";
+
+export type FromKey = "query" | "params" | "body" | "headers";
+export interface FromOptions {
+  key: string;
+}
+export type FromResult = ReturnType<(typeof z)["meta"]>;
+
+export type ExtendResult = (typeof z) & {
+  from(from: FromKey, options?: FromOptions): FromResult;
+};
+
+export function extendZod(zod: typeof z): ExtendResult {
+  if (typeof zod.ZodType.prototype.from !== "undefined") {
+    // This zod instance is already extended with the required methods,
+    // doing it again will just result in multiple wrapper methods
+    return zod as ExtendResult;
+  }
+
+  zod.ZodType.prototype.from = function (
+    from: FromKey,
+    options?: FromOptions
+  ): FromResult {
+    return this.meta({
+      from,
+      ...(options ?? {})
+    });
+  };
+
+  return zod as ExtendResult;
+}
+
+const zod = extendZod(z);
+
+export {
+  zod,
+  zod as z
+};
+
+export default zod;
