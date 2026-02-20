3db5c53eeba938c94a4d16a5353d0156ed99e39f
Yurii Hulyk <URiiGulick32@gmail.com>
2026-02-20 10:24:34 +0000

    feat(drizzle-model): re-export everything from package

--- packages/drizzle-model/src/index.ts
+++ packages/drizzle-model/src/index.ts
@@ -1,1 +1,2 @@
 export * from "./model/index.ts";
+export * from "./types";
--- packages/drizzle-model/src/model/index.ts
+++ packages/drizzle-model/src/model/index.ts
@@ -1,1 +1,15 @@
 export * from "./builder.ts";
+export * from "./config";
+export * from "./dialect.ts";
+export * from "./foreigns.ts";
+export * from "./format.ts";
+// Methods
+export * from "./methods";
+export * from "./model.ts";
+export * from "./options.ts";
+// Query
+export * from "./query/operations.ts";
+export * from "./relation.ts";
+export * from "./result.ts";
+export * from "./shape.ts";
+export * from "./table.ts";
